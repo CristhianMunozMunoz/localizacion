4507c5fa083b17ffb3cdf5edfcc4cd4ca84c53b2
Cristhian Muñoz <cristian.munoz@cumplimos.co>
2022-07-25 15:24:11 -0500

    quitar console log

--- src/componets/MapView.jsx
+++ src/componets/MapView.jsx
@@ -14,8 +14,8 @@ const MapView = () => {
   useEffect(() => {
     navigator.geolocation.getCurrentPosition(
       function (position) {
-        console.log(position.coords.longitude);
-        console.log(position.coords.latitude);
+        /*         console.log(position.coords.longitude);
+        console.log(position.coords.latitude); */
         setUbicacion({
           latitude: position.coords.latitude,
           longitude: position.coords.longitude,
