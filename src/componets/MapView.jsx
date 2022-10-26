b24a19a61f829993fc2b71ff0821d97566b3e69a
CristhianMunozMunoz <cristian.munoz@cumplimos.co>
2022-10-25 19:35:42 -0500

    cambiar zoom

--- src/componets/MapView.jsx
+++ src/componets/MapView.jsx
@@ -40,7 +40,7 @@ const MapView = () => {
             lat: ubicacion.latitude,
             lng: ubicacion.longitude,
           }}
-          zoom={20}
+          zoom={15}
           /*    scrollWheelZoom={true} */
           style={{ width: "100vw", height: "100vh" }}
         >
